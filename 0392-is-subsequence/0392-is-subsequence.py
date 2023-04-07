6f7993fc769e31ba64f8d70da179d23f7d085291
romcanrom <85007104+romcanrom@users.noreply.github.com>
2023-04-08 01:43:39 +0900

    Time: 33 ms (64.75%), Space: 14 MB (8.37%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,6 +2,8 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) == 0:
             return True
+        if len(s) > len(t):
+            return False
         idx = 0
         for tt in t:
             if tt == s[idx]:
