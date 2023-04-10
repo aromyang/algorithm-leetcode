52182de79e24d79f3d3b8395de9eddcb1a7f36dd
romcanrom <85007104+romcanrom@users.noreply.github.com>
2023-04-11 00:20:01 +0900

    Time: 39 ms (57.45%), Space: 13.9 MB (65.51%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,42 +5,41 @@
 #         self.next = next
 
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> ListNode:    
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> ListNode:
         if not list1 or not list2:
             return list1 or list2
+        
+        def create_node(val):
+            return ListNode(val=val, next=None)
+        
+        def add_node(node, val):
+            new_node = create_node(val)
+            node.next = new_node
+            return new_node
 
         ans = None
+        cur = None
 
         while list1 or list2:
             if not list1:
-                cur = ans
-                while cur.next:
-                    cur = cur.next
-                cur.next = list2
-                return ans
+                cur = add_node(cur, list2.val)
+                list2 = list2.next
             elif not list2:
-                cur = ans
-                while cur.next:
-                    cur = cur.next
-                cur.next = list1
-                return ans
-            if list1.val <= list2.val:
+                cur = add_node(cur, list1.val)
+                list1 = list1.next
+            elif list1.val <= list2.val:
                 if not ans:
-                    ans = ListNode(val=list1.val, next=None)
-                else:
+                    ans = create_node(list1.val)
                     cur = ans
-                    while cur.next:
-                        cur = cur.next
-                    cur.next = ListNode(val=list1.val, next=None)
+                else:
+                    cur = add_node(cur, list1.val)
                 list1 = list1.next
             else:
                 if not ans:
-                    ans = ListNode(val=list2.val, next=None)
-                else:
+                    ans = create_node(list2.val)
                     cur = ans
-                    while cur.next:
-                        cur = cur.next
-                    cur.next = ListNode(val=list2.val, next=None)
+                else:
+                    cur = add_node(cur, list2.val)
                 list2 = list2.next
         return ans
 
