c893e2aa24130be324d95aa068e6d521ec91b1bf
romcanrom <85007104+romcanrom@users.noreply.github.com>
2023-04-15 00:22:51 +0900

    Time: 35 ms (56.80%), Space: 13.8 MB (95.07%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        nums = {}
+        for ss in s:
+            nums[ss] = 1 + nums.get(ss, 0)
+        
+        print(nums)
+        
+        palindrome = 0
+        has_odd = False
+        for value in nums.values():
+            if value & 1: 
+                has_odd = True
+                palindrome += (value - 1)
+            else:
+                palindrome += value
+        
+        return palindrome + 1 if has_odd else palindrome
