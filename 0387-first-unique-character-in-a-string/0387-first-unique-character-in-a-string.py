f089475479bf721bfa4372ffa4a9c674628c57bb
romcanrom <85007104+romcanrom@users.noreply.github.com>
2023-04-26 23:16:19 +0900

    Time: 133 ms (36.79%), Space: 16.6 MB (6.21%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,12 +1,7 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        alphas = {}
         for i, ss in enumerate(s):
-            if ss not in alphas:
-                alphas[ss] = i
-            else:
-                alphas[ss] = -1
+            if s.index(ss) == s.rindex(ss):
+                return i
         
-        valid_idxes = [idx for idx in alphas.values() if idx != -1]
-
-        return -1 if not valid_idxes else min(valid_idxes)
+        return -1
