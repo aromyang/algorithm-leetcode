f6eb8e61f07384ea5533fa4253659c1ca04ce00e
aromyang <85007104+aromyang@users.noreply.github.com>
2023-05-15 22:32:49 +0900

    Time: 61 ms (7.46%), Space: 16.4 MB (10.49%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,17 +1,16 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        answer = ""
+        answer = []
         stack = []
-        primitive = []
+        start = 0
         
-        for ss in s:
-            primitive.append(ss)
-            if ss == '(':
-                stack.append('(')
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
             else:
+                if len(stack) == 1:
+                    answer.append(s[start+1:i])
+                    start = i + 1
                 stack.pop()
-                if not stack:
-                    answer += ''.join(primitive[1:-1])
-                    primitive.clear()
-        
-        return answer
+                
+        return ''.join(answer)
