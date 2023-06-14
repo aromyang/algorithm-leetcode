949a82aab752e6cf5497fd7632ff78d1f020e53c
aromyang <85007104+aromyang@users.noreply.github.com>
2023-06-14 21:44:45 +0900

    Time: 81 ms (71.43%), Space: 17.5 MB (40.58%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,24 +1,7 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
-        maxHeap = []
-        for i, s in enumerate(score):
-            heappush(maxHeap, (-s, i))
-            
-        answer = [0] * len(score)
-        place = 1
-        
-        while maxHeap:
-            idx = heappop(maxHeap)[1]
-            if place > 3:
-                rank = str(place)
-            elif place == 1:
-                rank = "Gold Medal"
-            elif place == 2:
-                rank = "Silver Medal"
-            elif place == 3:
-                rank = "Bronze Medal"
-                
-            answer[idx] = rank
-            place += 1
-            
-        return answer
+        rank = ["Gold Medal", "Silver Medal", "Bronze Medal"] + list(map(str, range(4, len(score) + 1)))
+        place = sorted(score, reverse = True)
+        d = dict(zip(place, rank))
+
+        return [d.get(s) for s in score]
