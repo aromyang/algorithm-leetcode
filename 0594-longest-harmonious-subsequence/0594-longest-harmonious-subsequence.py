55ff80121a1c8d52ffed2912c162b5035ff940f1
aromyang <85007104+aromyang@users.noreply.github.com>
2023-07-03 22:33:50 +0900

    Time: 303 ms (71.18%), Space: 18.2 MB (66.62%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -4,7 +4,7 @@ class Solution:
         ans = 0
 
         for cnt in cnts:
-            ans = max(ans, max(cnts.get(cnt) + cnts.get(cnt - 1, -cnts.get(cnt)), 
-                               cnts.get(cnt) + cnts.get(cnt + 1, -cnts.get(cnt))))
+            if cnt + 1 in cnts:
+                ans = max(ans, cnts.get(cnt) + cnts.get(cnt + 1))
             
         return ans
