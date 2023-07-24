684e3b0ecca970b14982501ebd8e07adb4cc1115
aromyang <85007104+aromyang@users.noreply.github.com>
2023-07-24 22:05:46 +0900

    Time: 47 ms (60.28%), Space: 16.2 MB (68.55%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,8 +1,3 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        if word == word.upper() or word == word.lower():
-            return True
-        elif word[:1] == word[:1].upper() and word[1:] == word[1:].lower():
-            return True
-        else:
-            return False
+        return True if word.islower() or word.istitle() or word.isupper() else False
