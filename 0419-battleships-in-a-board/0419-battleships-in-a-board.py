746ac64e7d3b7e80705bcbf594239fa4400785b5
aromyang <85007104+aromyang@users.noreply.github.com>
2023-10-19 21:36:54 +0900

    Time: 85 ms (33.53%), Space: 16.7 MB (87.53%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -1,25 +1,16 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
         x_limit, y_limit = len(board), len(board[0])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = [[False] * y_limit for _ in range(x_limit)]
-        
-        def dfs(cur_x, cur_y):
-            nonlocal x_limit, y_limit, directions, visited
-            visited[cur_x][cur_y] = True
-            
-            for dx, dy in directions:
-                next_x, next_y = cur_x + dx, cur_y + dy
-                if 0 <= next_x < x_limit and 0 <= next_y < y_limit:
-                    if board[next_x][next_y] == 'X' and not visited[next_x][next_y]:
-                        dfs(next_x, next_y)
-            
-            
         ans = 0
+        
         for x in range(x_limit):
             for y in range(y_limit):
-                if board[x][y] == 'X' and not visited[x][y]:
-                    dfs(x, y)
-                    ans += 1
+                if board[x][y] == '.':
+                    continue
+                if x > 0 and board[x-1][y] == 'X':
+                    continue
+                if y > 0 and board[x][y-1] == 'X':
+                    continue
+                ans += 1
         
         return ans
