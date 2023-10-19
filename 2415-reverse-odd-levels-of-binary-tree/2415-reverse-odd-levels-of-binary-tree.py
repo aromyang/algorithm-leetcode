fda493eef16e231e485ba23bcd538e9fa33d26b4
aromyang <85007104+aromyang@users.noreply.github.com>
2023-10-19 21:12:45 +0900

    Time: 789 ms (37.59%), Space: 22.5 MB (87.78%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -6,23 +6,16 @@
 #         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.depth_dict = defaultdict(list)
         
-        def dfs(cur_node, cur_depth):
-            if not cur_node:
+        def dfs(left_node, right_node, depth):
+            if not left_node or not right_node:
                 return
+            if depth & 1:
+                left_node.val, right_node.val = right_node.val, left_node.val
             
-            if cur_depth & 1:
-                self.depth_dict[cur_depth].append(cur_node)
-            
-            dfs(cur_node.left, cur_depth + 1)
-            dfs(cur_node.right, cur_depth + 1)
+            dfs(left_node.left, right_node.right, depth + 1)
+            dfs(left_node.right, right_node.left, depth + 1)
         
-        dfs(root, 0)
-        
-        for depth, nodes in self.depth_dict.items():
-            reversed_val = [node.val for node in reversed(nodes)]
-            for i, node in enumerate(nodes):
-                node.val = reversed_val[i]
+        dfs(root.left, root.right, 1)
         
         return root
