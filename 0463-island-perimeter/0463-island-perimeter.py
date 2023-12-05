5883da0fa901e550df7740616c716496396e7cf9
aromyang <85007104+aromyang@users.noreply.github.com>
2023-12-05 21:29:36 +0900

    Time: 488 ms (47.19%), Space: 16.7 MB (46.99%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,25 +1,29 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+        ans = 0
         x_limit, y_limit = len(grid), len(grid[0])
         visited = [[False] * len(grid[0]) for _ in range(len(grid))]
         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        ans = 0
+        q = deque()
         
-        def dfs(cur_x, cur_y):
-            nonlocal ans
-            visited[cur_x][cur_y] = True
-            for dx, dy in directions:
-                next_x, next_y = cur_x + dx, cur_y + dy
-                if 0 <= next_x < x_limit and 0 <= next_y < y_limit:
-                    if grid[next_x][next_y] == 1 and not visited[next_x][next_y]:
-                        dfs(next_x, next_y)
-                    elif grid[next_x][next_y] == 0:
-                        ans += 1
-                else:
-                    ans += 1
-                        
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
+        for x in range(x_limit):
+            for y in range(y_limit):
                 if grid[x][y] == 1:
-                    dfs(x, y)
+                    visited[x][y] = True
+                    q.append((x, y))
+                    
+                    while q:
+                        cur_x, cur_y = q.popleft()
+
+                        for dx, dy in directions:
+                            next_x, next_y = cur_x + dx, cur_y + dy
+                            if 0 <= next_x < x_limit and 0 <= next_y < y_limit:
+                                if grid[next_x][next_y] == 1:
+                                    if not visited[next_x][next_y]:
+                                        visited[next_x][next_y] = True
+                                        q.append((next_x, next_y))
+                                    ans -= 1
+                        
+                        ans += 4
+
                     return ans
