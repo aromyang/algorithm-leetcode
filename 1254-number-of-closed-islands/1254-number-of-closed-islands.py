e27320f3f4c1ff8d36f860eda12dd8f4a7d78397
aromyang <85007104+aromyang@users.noreply.github.com>
2023-12-21 20:46:24 +0900

    Time: 109 ms (93.34%), Space: 17.7 MB (28.54%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -5,28 +5,32 @@ class Solution:
         visited = [[False] * limit_y for _ in range(limit_x)]
         ans = 0
         
-        def dfs(cur_x, cur_y):
-            if cur_x == 0 or cur_x == limit_x - 1 or cur_y == 0 or cur_y == limit_y - 1:
-                if grid[cur_x][cur_y] == 0:
-                    return False
-            
-            if grid[cur_x][cur_y] == 1 or visited[cur_x][cur_y]:
+        def bfs(start_x, start_y):
+            if grid[start_x][start_y] == 1:
                 return True
             
-            visited[cur_x][cur_y] = True
-            is_islands = True
-            
-            for dx, dy in directions:
-                next_x, next_y = cur_x + dx, cur_y + dy
-                if 0 <= next_x < limit_x and 0 <= next_y < limit_y:
-                    is_islands &= dfs(next_x, next_y)
-                        
-            return is_islands
+            q = deque([(start_x, start_y)])
+            is_island = True
+
+            while q:
+                cur_x, cur_y = q.popleft()
+                if cur_x == 0 or cur_x == limit_x - 1 or cur_y == 0 or cur_y == limit_y - 1:
+                    if grid[cur_x][cur_y] == 0:
+                        is_island = False
+
+                for dx, dy in directions:
+                    next_x, next_y = cur_x + dx, cur_y + dy
+                    if 0 <= next_x < limit_x and 0 <= next_y < limit_y:
+                        if not visited[next_x][next_y] and grid[next_x][next_y] == 0:
+                            visited[next_x][next_y] = True
+                            q.append((next_x, next_y))
+
+            return 1 if is_island else 0
                                 
         for x in range(limit_x):
             for y in range(limit_y):
                 if not visited[x][y] and grid[x][y] == 0:
-                    if dfs(x, y):
-                        ans += 1
+                    visited[x][y] = True
+                    ans += bfs(x, y)
         
         return ans
