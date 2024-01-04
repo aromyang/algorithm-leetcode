2529e77b6eeed5a91fb48c97fdd26f72c2f1e929
aromyang <85007104+aromyang@users.noreply.github.com>
2024-01-04 22:03:01 +0900

    Time: 42 ms (66.16%), Space: 17.3 MB (10.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -8,39 +8,28 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> ListNode:
         if not list1 or not list2:
             return list1 or list2
-        
-        def create_node(val):
-            return ListNode(val=val, next=None)
-        
-        def add_node(node, val):
-            new_node = create_node(val)
-            node.next = new_node
-            return new_node
 
-        ans = None
-        cur = None
-
-        while list1 or list2:
-            if not list1:
-                cur.next = list2
-                break
-            elif not list2:
-                cur.next = list1
-                break
-            elif list1.val <= list2.val:
-                if not ans:
-                    ans = create_node(list1.val)
-                    cur = ans
-                else:
-                    cur = add_node(cur, list1.val)
-                list1 = list1.next
+        def merge(list1, list2):
+            if not list1 or not list2:
+                return list1 or list2
+            
+            if list1.val <= list2.val:
+                list1.next = merge(list1.next, list2)
+                return list1
             else:
-                if not ans:
-                    ans = create_node(list2.val)
-                    cur = ans
-                else:
-                    cur = add_node(cur, list2.val)
-                list2 = list2.next
+                list2.next = merge(list1, list2.next)
+                return list2
+                
+        
+        if list1.val <= list2.val:
+            ans = list1
+            list1 = list1.next
+        else:
+            ans = list2
+            list2 = list2.next
+
+        ans.next = merge(list1, list2)
+        
         return ans
 
                 
