d83aa80775747d05a075c56331b7e90e94e219a0
아롬 <85007104+aromyang@users.noreply.github.com>
2024-06-18 21:46:01 +0900

    Time: 414 ms (5.41%), Space: 44.6 MB (9.01%) - LeetHub

--- 1238-circular-permutation-in-binary-representation/1238-circular-permutation-in-binary-representation.py
+++ 1238-circular-permutation-in-binary-representation/1238-circular-permutation-in-binary-representation.py
@@ -1,5 +1,30 @@
 class Solution:
     def circularPermutation(self, n: int, start: int) -> List[int]:
-        gray_codes = [i ^ (i >>1) for i in range(2 ** n)]
-        start_idx = gray_codes.index(start)
-        return gray_codes[start_idx:] + gray_codes[:start_idx]
+        
+        def backtrack(visited, path, prev_str, goal):
+            if len(visited) == goal:
+                return True
+            
+            for i in range(n):
+                new_str = prev_str[:i] + str(1 - int(prev_str[i])) + prev_str[i + 1:]
+                if new_str not in visited:
+                    visited.add(new_str)
+                    path.append(new_str)
+                    
+                    if backtrack(visited, path, new_str, goal):
+                        return True
+                    
+                    visited.set.remove(new_str)
+                    path.pop()
+            return False
+        
+        sys.setrecursionlimit(1000000)
+        
+        visited = set()
+        initial_str = bin(start)[2:].zfill(n)
+        path = [initial_str]
+        visited.add(initial_str)
+        
+        backtrack(visited, path, initial_str, 2 ** n)
+
+        return [int(binary_str, 2) for binary_str in path]
